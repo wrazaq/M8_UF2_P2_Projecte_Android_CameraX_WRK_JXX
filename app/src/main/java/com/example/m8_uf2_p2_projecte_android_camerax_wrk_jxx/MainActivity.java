c69be938515973bb74b924052e91dbb7b7a24143
wrazaq <waleed.razaq2000@gmail.com>
2024-02-17 10:23:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/m8_uf2_p2_projecte_android_camerax_wrk_jxx/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.m8_uf2_p2_projecte_android_camerax_wrk_jxx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "M8_UF2_P2_Projecte_Android_CameraX_WRK_JXX"
+include(":app")
+ 
