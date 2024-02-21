0034f92b03e78ed9bb3db6761670e809f6df1fc9
wrazaq <waleed.razaq2000@gmail.com>
2024-02-21 16:12:56 +0100

    added functionality to save image to local storage, added preview for the picture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("androidx.camera:camera-view:1.3.1")
     implementation("androidx.camera:camera-mlkit-vision:${camerax_version}")
     implementation("androidx.camera:camera-extensions:${camerax_version}")
+    implementation("com.squareup.picasso:picasso:2.71828")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/m8_uf2_p2_projecte_android_camerax_wrk_jxx/MainActivity.java
+++ app/src/main/java/com/example/m8_uf2_p2_projecte_android_camerax_wrk_jxx/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.AspectRatio;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
@@ -16,17 +15,28 @@ import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Environment;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
+
 
 import com.example.m8_uf2_p2_projecte_android_camerax_wrk_jxx.databinding.ActivityMainBinding;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
@@ -34,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     ImageButton cameraBtn, recordBtn, flipBtn, toggleFlash;
     private PreviewView previewView;
+    private ImageView previewImageView;
     int cameraOrientation = CameraSelector.LENS_FACING_BACK;
     private final ActivityResultLauncher<String> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
         @Override
@@ -50,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         previewView = binding.cameraPreview;
+        previewImageView = binding.previewImageView;
         cameraBtn = binding.cameraBtn;
         flipBtn = binding.flipCamera;
         toggleFlash = binding.toggleFlash;
@@ -69,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
             startCamera(cameraOrientation);
         });
     }
-    public void startCamera(int cameraOrientation) {
+    private void startCamera(int cameraOrientation) {
         ListenableFuture<ProcessCameraProvider> listenableFuture = ProcessCameraProvider.getInstance(this);
 
         listenableFuture.addListener(() -> {
@@ -100,24 +112,69 @@ public class MainActivity extends AppCompatActivity {
             }
         }, ContextCompat.getMainExecutor(this));
     }
-    public void takePicture(ImageCapture imageCapture) {
+    private void takePicture(ImageCapture imageCapture) {
         final File file = new File(getExternalFilesDir(null), System.currentTimeMillis() + ".jpg");
         ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(file).build();
         imageCapture.takePicture(outputFileOptions, Executors.newCachedThreadPool(), new ImageCapture.OnImageSavedCallback() {
             @Override
             public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
                 runOnUiThread(() -> {
-                    Toast.makeText(MainActivity.this, "Image saved at: " + file.getPath(), Toast.LENGTH_SHORT).show();
+                    saveImageToMediaStore(file);
+
                     startCamera(cameraOrientation);
+                    showPreview(file.getPath());
                 });
             }
+
             @Override
             public void onError(@NonNull ImageCaptureException exception) {
-                    Toast.makeText(MainActivity.this, "Failed to save: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
-                    startCamera(cameraOrientation);
+                Toast.makeText(MainActivity.this, "Failed to save: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                startCamera(cameraOrientation);
             }
         });
     }
+
+    private void saveImageToMediaStore(File file) {
+        ContentResolver contentResolver = getContentResolver();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, file.getName());
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + File.separator);
+
+        Uri imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+        try {
+            if (imageUri != null) {
+                try (OutputStream outputStream = contentResolver.openOutputStream(imageUri);
+                     FileInputStream inputStream = new FileInputStream(file)) {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        assert outputStream != null;
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showPreview(String imagePath) {
+        previewImageView.setVisibility(View.VISIBLE);
+
+        Picasso.get()
+                .load(new File(imagePath))
+                .into(previewImageView);
+
+        new Handler().postDelayed(() -> {
+            previewImageView.setVisibility(View.GONE);
+            startCamera(cameraOrientation);
+        }, 10000);
+    }
+
+
+
     private void setFlashIcon(Camera camera) {
         if (camera.getCameraInfo().hasFlashUnit()) {
             if (camera.getCameraInfo().getTorchState().getValue() == 0) {
